da032df136c2caae03ed3b0c25f35defe3153427
Maisorpt <maisorpt@gmail.com>
2026-06-19 14:25:25 +0700

    fixing route on backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const { getUserProfile } = require('../controllers/userController'); 
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Baris 8 lo yang memicu eror:
 router.get('/profile', authMiddleware, getUserProfile); 
 
 module.exports = router;
